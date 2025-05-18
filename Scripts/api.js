2ada9f74b44058b84376210937c83594a8ba9129
HSMathebula <mathebulahappen8@gmail.com>
2025-05-18 22:43:41 +0200

    update server file

--- Scripts/api.js
+++ Scripts/api.js
@@ -1,7 +1,7 @@
 // API URL configuration
 const API_URL = window.location.hostname === 'localhost' 
     ? 'http://localhost:3000/api'
-    : 'https://quiz-app-db.onrender.com/api';
+    : 'https://quiz-app-db-2.onrender.com/api';
 
 // Get categories
 async function getCategories() {
