ada8ec9c497b3d01bcab61803fde6758e733bd3d
HSMathebula <mathebulahappen8@gmail.com>
2025-05-18 22:26:13 +0200

    update server file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const db = require('./Database/db.js');
+const db = require('./Database/db');
 require('dotenv').config();
 
 const app = express();
